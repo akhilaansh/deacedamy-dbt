a45bb3b8dc1e3d0cf185cb5b22a6fbdbf7e0b8ac
Akhila Chowdary Annamareddy <akhila.workbd@gmail.com>
2025-07-23 12:16:45 +0000

    final changes

--- models/metrics/avg_nurse_to_patient_ratio.sql
+++ models/metrics/avg_nurse_to_patient_ratio.sql
@@ -1,9 +1,16 @@
+-- select
+--     provnum,
+--     state,
+--     'RN' as department,
+--     avg(hrs_rn / nullif(patient_count, 0)) as avg_rn_ratio,
+--     avg(hrs_lpn / nullif(patient_count, 0)) as avg_lpn_ratio,
+--     avg(hrs_cna / nullif(patient_count, 0)) as avg_cna_ratio
+-- from {{ ref('stg_nurse_staffing') }}
+-- group by 1, 2, 3
+
 select
     provnum,
     state,
-    'RN' as department,
-    avg(hrs_rn / nullif(patient_count, 0)) as avg_rn_ratio,
-    avg(hrs_lpn / nullif(patient_count, 0)) as avg_lpn_ratio,
-    avg(hrs_cna / nullif(patient_count, 0)) as avg_cna_ratio
+    avg( (hrs_rn + hrs_lpn + hrs_cna) / nullif(patient_count, 0) ) as avg_nurse_to_patient_ratio
 from {{ ref('stg_nurse_staffing') }}
-group by 1, 2, 3
+group by 1, 2
